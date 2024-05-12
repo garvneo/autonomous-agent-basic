4d78d7e7946e6d36b23cd289be2a87bb49aa55d5
unknown <gauravchandsingh42@gmail.com>
2024-05-13 05:12:17 +0530

    handled autonomous agents asynchronously

---
+++ src/configs/config.py
@@ -0,0 +1,18 @@
+"""
+config.py
+
+This module holds all configs and constants that are used in other modules.
+"""
+
+ALPHABET = [
+    "hello",
+    "sun",
+    "world",
+    "space",
+    "moon",
+    "crypto",
+    "sky",
+    "ocean",
+    "universe",
+    "human",
+]
